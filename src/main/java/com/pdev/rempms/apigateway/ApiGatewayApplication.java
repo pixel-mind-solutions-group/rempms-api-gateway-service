049548f6c8854c1353b35396b12ec18e23c44a9a
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-07-27 06:41:50 +0530

    confige resiliencve4j

--- src/main/java/com/pdev/rempms/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/pdev/rempms/apigateway/ApiGatewayApplication.java
@@ -3,22 +3,38 @@ package com.pdev.rempms.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
-import java.net.InetAddress;
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
 
-		try {
-			InetAddress address = InetAddress.getByName("DESKTOP-681N2R8.mshome.net");
-			System.out.println("IP Address: " + address.getHostAddress());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    }
 
+    @Bean
+    public RouteLocator routeConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("location-service-route", p -> p
+                        .path("/api/location/**")
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                                // circuit breaker pattern
+                                .circuitBreaker(config -> config
+                                        .setName("locationCircuitBreaker")
+                                        .setFallbackUri("forward:/fallback/response/message"))
+                                // retry pattern
+                                .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+                        )
+                        .uri("lb://REMPMS-LOCATION-SERVICE"))
+                .build();
+    }
 }
