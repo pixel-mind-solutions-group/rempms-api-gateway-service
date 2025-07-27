34ca81c52a2e535f3e65981ef658178aceb10189
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-07-27 12:24:00 +0530

    configuring routings

--- src/main/java/com/pdev/rempms/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/pdev/rempms/apigateway/ApiGatewayApplication.java
@@ -35,6 +35,58 @@ public class ApiGatewayApplication {
                                         .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
                         )
                         .uri("lb://REMPMS-LOCATION-SERVICE"))
+                .route("draft-service-route", p -> p
+                        .path("/api/draft/**")
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                                // circuit breaker pattern
+                                .circuitBreaker(config -> config
+                                        .setName("draftCircuitBreaker")
+                                        .setFallbackUri("forward:/fallback/response/message"))
+                                // retry pattern
+                                .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+                        )
+                        .uri("lb://REMPMS-DRAFT-SERVICE"))
+                .route("document-service-route", p -> p
+                        .path("/api/document/**")
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                                // circuit breaker pattern
+                                .circuitBreaker(config -> config
+                                        .setName("documentCircuitBreaker")
+                                        .setFallbackUri("forward:/fallback/response/message"))
+                                // retry pattern
+                                .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+                        )
+                        .uri("lb://REMPMS-DOCUMENT-SERVICE"))
+                .route("communication-service-route", p -> p
+                        .path("/api/communication/**")
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                                // circuit breaker pattern
+                                .circuitBreaker(config -> config
+                                        .setName("communicationCircuitBreaker")
+                                        .setFallbackUri("forward:/fallback/response/message"))
+                                // retry pattern
+                                .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+                        )
+                        .uri("lb://REMPMS-COMMUNICATION-SERVICE"))
+                .route("candidate-service-route", p -> p
+                        .path("/api/candidate/**")
+                        .filters(f -> f
+                                .rewritePath("/(?<segment>.*)", "/${segment}")
+                                // circuit breaker pattern
+                                .circuitBreaker(config -> config
+                                        .setName("candidateCircuitBreaker")
+                                        .setFallbackUri("forward:/fallback/response/message"))
+                                // retry pattern
+                                .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true))
+                        )
+                        .uri("lb://REMPMS-CANDIDATE-SERVICE"))
                 .route("recruitment-service-route", p -> p
                         .path("/api/recruitment/**")
                         .filters(f -> f
